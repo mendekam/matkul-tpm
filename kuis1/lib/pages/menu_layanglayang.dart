f89add944d8fda8858a410ebf90f0a3c79bd5e12
Abid Bilal <abiddddbilal@gmail.com>
2023-03-14 14:31:15 +0700

    intial commit

---
+++ kuis1/lib/pages/menu_layanglayang.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MenuLayangLayang extends StatefulWidget {
+  const MenuLayangLayang({super.key});
+
+  @override
+  State<MenuLayangLayang> createState() => _MenuLayangLayangState();
+}
+
+class _MenuLayangLayangState extends State<MenuLayangLayang> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      
+    );
+  }
+}
---
+++ kuis1/lib/pages/menu_segitiga.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MenuSegitiga extends StatefulWidget {
+  const MenuSegitiga({super.key});
+
+  @override
+  State<MenuSegitiga> createState() => _MenuSegitigaState();
+}
+
+class _MenuSegitigaState extends State<MenuSegitiga> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
