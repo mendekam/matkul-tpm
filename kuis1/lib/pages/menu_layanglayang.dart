cc25d60f00fdf86fc8ca4aa250b64461a4f3ce7d
Abid Bilal <abiddddbilal@gmail.com>
2023-03-14 15:32:49 +0700

    luas segitiga dan layanglayang

--- kuis1/lib/pages/menu_layanglayang.dart
+++ kuis1/lib/pages/menu_layanglayang.dart
@@ -10,10 +10,76 @@ class MenuLayangLayang extends StatefulWidget {
 }
 
 class _MenuLayangLayangState extends State<MenuLayangLayang> {
+  final TextEditingController diagonal1Controller = TextEditingController();
+  final TextEditingController diagonal2Controller = TextEditingController();
+  String hasil = "";
+  double diagonal1 = 0;
+  double diagonal2 = 0;
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      
+    return Scaffold(
+      body: Center(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 70),
+                child: Image.asset('assets/layanglayang.jpg', height: 200, width: 200,),
+              ),
+              Container(
+                padding: const EdgeInsets.all(20),
+                child: Text(hasil),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                child: TextField(
+                  controller: diagonal1Controller,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(90.0),
+                    ),
+                    labelText: 'Diagonal 1',
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                child: TextField(
+                  controller: diagonal2Controller,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(90.0),
+                    ),
+                    labelText: 'Diagonal 2',
+                  ),
+                ),
+              ),
+              Container(
+                  height: 80,
+                  padding: const EdgeInsets.all(20),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(50),
+                    ),
+                    child: const Text('Hitung'),
+                    onPressed: luasLayangLayang,
+                  )),
+            ]),
+      ),
     );
+  }
+
+  void luasLayangLayang() {
+    double luas;
+    diagonal1 = double.parse(diagonal1Controller.text);
+    diagonal2 = double.parse(diagonal2Controller.text);
+
+    luas = 0.5 * diagonal1 * diagonal2 ;
+
+    setState(() {
+      hasil = luas.toString();
+    });
   }
 }
--- kuis1/lib/pages/menu_segitiga.dart
+++ kuis1/lib/pages/menu_segitiga.dart
@@ -10,10 +10,76 @@ class MenuSegitiga extends StatefulWidget {
 }
 
 class _MenuSegitigaState extends State<MenuSegitiga> {
+  final TextEditingController widthController = TextEditingController();
+  final TextEditingController heightController = TextEditingController();
+  String hasil = "";
+  double width = 0;
+  double height = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Center(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 70),
+                child: Image.asset('assets/segitiga.jpg', height: 200, width: 200,),
+              ),
+              Container(
+                padding: const EdgeInsets.all(20),
+                child: Text(hasil),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                child: TextField(
+                  controller: widthController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(90.0),
+                    ),
+                    labelText: 'Panjang Alas',
+                  ),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                child: TextField(
+                  controller: heightController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(90.0),
+                    ),
+                    labelText: 'Tinggi',
+                  ),
+                ),
+              ),
+              Container(
+                  height: 80,
+                  padding: const EdgeInsets.all(20),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      minimumSize: const Size.fromHeight(50),
+                    ),
+                    child: const Text('Hitung'),
+                    onPressed: luasSegitiga,
+                  )),
+            ]),
+      ),
     );
+  }
+
+  void luasSegitiga() {
+    double luas;
+    width = double.parse(widthController.text);
+    height = double.parse(heightController.text);
+
+    luas = height * width / 2;
+
+    setState(() {
+      hasil = luas.toString();
+    });
   }
 }
